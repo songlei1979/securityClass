65b8be535149008f7b1466e83b6b808d67d70ef3
Lei <gabriel_sl19798@hotmail.com>
2020-06-02 11:35:26 +1200

    show query

--- loginForm.php
+++ loginForm.php
@@ -3,6 +3,6 @@
 
 <form method="post" action="login.php">
     <p>Username: <input type="text" name="username"></p>
-    <p>Password: <input type="password" name="password"></p>
+    <p>Password: <input type="text" name="password"></p>
     <p><input type="submit" value="Login"></p>
 </form>
